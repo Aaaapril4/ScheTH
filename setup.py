02428feed77f1f98f6824c41d2645b386b5eb7f6
Aaaapril4 <gia.jyq@gmail.com>
2022-01-28 17:09:36 -0500

    modify

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scheth",
-    version="0.0.2",
+    name="ScheTH",
+    version="1.0.0",
     author="YJie",
     author_email="jieyaqi@msu.edu",
     description="A package for generating detailed study plan",
